9086b52066bdc8c9a67ee829e585d94ef1a4d265
Ben Schwarz <ben@Spinners.local>
2008-06-12 21:20:06 +1000

    Added additional specs (focus: Headers returned for action caches)

---
+++ spec/action_cache_spec.rb
@@ -0,0 +1,13 @@
+#dispatch_to(Merb::Test::Fixtures::Controllers::DisplayWithStringLocation, :index, {}, :http_accept => "application/json").headers['Location'].should =~ /some_resources/
+
+describe "merb action cache returned headers" do
+  it "should return gzip"
+  it "should not return gzip"
+  it "should set cache-control"
+  it "should set expires"
+  it "should set etag"
+  it "should set last-modified"
+  it "should set the expires header to the current time and date plus the defined cache expiry time"
+  it "should return gzip if the client can accept it"
+  it "should not return gzip when the client can not accept it"
+end
---
+++ spec/merb_cache_spec.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+# Stitch the specs together
+require File.join(File.dirname(__FILE__), 'controller_spec')
+require File.join(File.dirname(__FILE__), 'action_cache_spec')
+
+# I refuse to spell initialisation with a z, this is me; taking it back for the rest of us.
+describe "merb-cache initialisation" do
+  it "should default to memcached cache store"
+  it "should error when an incorrect configuation value is mapped to the cache store"
+  it "should throw Merb::Cache:Store::NotFound for named cache stores that do not exist"
+end
+
+describe "returned caches" do
+  it "should return the format it was stored in"
+end
