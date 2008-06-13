1ab2173ed7af2a8aca8dc80cda139fc00583c86a
Ben Schwarz <ben.schwarz@gmail.com>
2008-06-13 23:06:40 +1000

    Adding basic specs to start filling out the memcached and mintcache stores

---
+++ spec/memcached_store_spec.rb
@@ -0,0 +1,9 @@
+describe "memcached store" do
+  it "should respond to :get"
+  it "should respond to :put"
+end
+
+describe "memcached store keys" do
+  it "should store keys in :controller/:action format by default"
+  it "should store keys in a specified format"
+end
---
+++ spec/mintcache_store_spec.rb
@@ -0,0 +1,12 @@
+describe "mintcache store" do
+  it "should respond to :get"
+  it "should respond to :put"
+end
+
+describe "mintache store avoiding the dogpile effect" do
+  it "should store a second key to keep check of the time" # "#{key}_validity"
+  it "should store a third key to keep the data for a longer expiry time" # "#{key}_data"
+  it "should use the second and third keys to return the data if the first key has expired"
+  it "should return a cache miss when the second level cache is used"
+  it "should set a temporary cache when the second level cache is used"
+end
